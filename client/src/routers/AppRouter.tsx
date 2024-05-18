02144c9274e579a5c31e6d336793bc4c70f2de4c
3ricLu <EricLu.Canada@gmail.com>
2024-05-18 15:10:59 -0400

    commit

--- client/src/routers/AppRouter.tsx
+++ client/src/routers/AppRouter.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import("../App.css");
 // Importing Pages
 import HomePage from "../pages/HomePage";
@@ -7,18 +7,55 @@ import ListingsPage from "../pages/ListingsPage";
 import GroupPage from "../pages/GroupPage";
 import ProfilePage from "../pages/ProfilePage";
 import EditProfilePage from "../pages/EditProfilePage";
+import CreateAccountForm from "../components/CreateAccountForm";
+import LoginForm from "../components/LoginForm";
+import { useEffect, useState } from "react";
 
 function AppRouter() {
   // Setting the default title
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  useEffect(() => {
+    // Check if the user is authenticated
+    const user = sessionStorage.getItem("user");
+    if (user) {
+      setIsAuthenticated(true);
+    }
+  }, []);
+
+  const handleLogin = (username: string) => {
+    sessionStorage.setItem("user", username);
+    setIsAuthenticated(true);
+  };
+
+  const handleLogout = () => {
+    sessionStorage.removeItem("user");
+    setIsAuthenticated(false);
+  };
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/listing" element={<ListingsPage />} />
         <Route path="/group" element={<GroupPage />} />
         <Route path="/profile" element={<ProfilePage />} />
-        <Route path="/edit-profile" element={<EditProfilePage/>}/>
+        <Route path="/edit-profile" element={<EditProfilePage />} />
+        <Route path="/create-account" element={<CreateAccountForm />} />
+        <Route path="/login" element={<LoginForm onLogin={handleLogin} />} />
+        <Route
+          path="/profile"
+          element={isAuthenticated ? <ProfilePage /> : <Navigate to="/login" />}
+        />
+        <Route
+          path="/"
+          element={
+            isAuthenticated ? (
+              <Navigate to="/profile" />
+            ) : (
+              <Navigate to="/login" />
+            )
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
